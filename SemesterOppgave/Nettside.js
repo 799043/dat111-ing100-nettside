b9bf84a516d3ed8b7c90012f3643755b34800ba3
LightyMatter <felixtvete@gmail.com>
2024-10-09 20:08:53 +0200

    Masse drit!

--- SemesterOppgave/Nettside.js
+++ SemesterOppgave/Nettside.js
@@ -1,10 +1,15 @@
 const hamMenu = document.querySelector('.ham-menu');
-
 const offScreenMenu = document.querySelector('.off-screen-menu');
+let previousScrollY = window.scrollY;
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
-})
+});
 
-const Home = document.querySelector('mBarHo');
+window.addEventListener('scroll', () => {
+    if (offScreenMenu.classList.contains('active')) {
+        offScreenMenu.classList.remove('active');
+        hamMenu.classList.remove('active');
+    }
+});
